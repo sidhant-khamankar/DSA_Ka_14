90011d1af3d9c4bea206a82a28daa1aff73f4d3f
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-07-08 01:11:09 +0530

    Time: 112 ms (77.19%), Space: 19.6 MB (77.37%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,19 @@
 from itertools import permutations
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Time O(m*nlogn)
+        # Space O(n)
         strs_table = {}
+        # O(m)
         for string in strs:
+            # O(nlogn)
             sorted_string = ''.join(sorted(string))
 
+            # O(1)
             if sorted_string not in strs_table:
                 strs_table[sorted_string] = []
 
+            # O(1)
             strs_table[sorted_string].append(string)
 
         return list(strs_table.values())
