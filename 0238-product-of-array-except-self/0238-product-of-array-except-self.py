17ea6f9a60a15ae0ee3852d07d86a81d41adc535
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-07-19 23:37:22 +0530

    Time: 237 ms (69.52%), Space: 24 MB (52.38%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,15 +3,22 @@ class Solution:
         # Approach 1 - Multiply all elements and then traverse array and divide by element at that index. O(n) time O(1) space
         
         # Approach 2 Neetcode - Postfix Aand prefix array calculated and then multiplied
-        # Can skip postfix and prefix array by putting prefix result in output array at one index greater than current index
+        # Can skip postfix and prefix array by putting prefix result in output array at one index greater than current index. 
         
         res = [1] * len(nums)
         
+        # prefix for 0th element
         prefix = 1
+        
+        # prefix result is stored at next adjacent position with reference to current position
         for i in range(len(nums)):
             res[i] = prefix
             prefix *= nums[i]
+            
+        # postfix for last element
         postfix = 1
+        
+        # multiply postfix and prefix starting from end
         for i in range(len(nums) - 1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
