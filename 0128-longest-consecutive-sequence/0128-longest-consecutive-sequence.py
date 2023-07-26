04beed040e85ceb562ee20bf7294c30bea86308f
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-07-26 20:07:47 +0530

    Time: 2020 ms (39.46%), Space: 31.2 MB (37.23%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,29 +1,46 @@
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+        
+#         # O(n) time O(1) space
+#         # drop duplicates
+#         # sort
+#         # check for consecutiveness
+#         # if consicutiveness breaks or it is last second element, then update maxi
+        
+#         nums = list(set(nums))
+#         if len(nums) == 0:
+#             return 0
+        
+#         nums.sort()
+#         curr = 1
+#         maxi = 1
+#         for i in range(0, len(nums)-1):
+            
+#             if i == len(nums)-2:
+#                 if (nums[i+1]==nums[i]+1):
+#                     curr += 1
+#                 maxi = max(curr, maxi) 
+#             elif nums[i+1] == nums[i] + 1:
+#                 curr += 1
+#             else:
+#                 maxi = max(curr, maxi)
+#                 curr = 1
+        
+#         return maxi
+    
+# Neetcode solution
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        
-        # O(n) time O(1) space
-        # drop duplicates
-        # sort
-        # check for consecutiveness
-        # if consicutiveness breaks or it is last second element, then update maxi
-        
-        nums = list(set(nums))
-        if len(nums) == 0:
-            return 0
-        
-        nums.sort()
-        curr = 1
-        maxi = 1
-        for i in range(0, len(nums)-1):
-            
-            if i == len(nums)-2:
-                if (nums[i+1]==nums[i]+1):
-                    curr += 1
-                maxi = max(curr, maxi) 
-            elif nums[i+1] == nums[i] + 1:
-                curr += 1
-            else:
-                maxi = max(curr, maxi)
-                curr = 1
-        
-        return maxi
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            # check if its the start of a sequence
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
+
+    
