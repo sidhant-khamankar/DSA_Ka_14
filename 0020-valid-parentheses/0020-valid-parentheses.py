b767181179a2b046fd41d020f888bb1dc7f4fedd
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-08-08 00:06:55 +0530

    Time: 50 ms (34.38%), Space: 16.2 MB (99.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Pure Stack
+    # My solution, Neetcode solution
+    # Time O(n) time O(n) space
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
