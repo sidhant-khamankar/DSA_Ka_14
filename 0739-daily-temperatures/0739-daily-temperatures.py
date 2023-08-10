db06f2f14700b19a4cb9230f0ee986c8f87bc2ea
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-08-10 11:33:04 +0530

    Time: 1119 ms (81.99%), Space: 33.1 MB (5.84%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,17 @@
+# My solution O(n^2) time O(n) space
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [None]*len(temperatures)
+        stack = list()
+        for index, temp in enumerate(temperatures):
+            while len(stack) != 0 and temp > stack[-1][1]:
+                ans[stack[-1][0]] = index - stack[-1][0]
+                stack.pop()
+            
+            stack.append([index,temp])
+        while len(stack) != 0:
+            ans[stack[-1][0]] = 0
+            stack.pop()
+        
+        return ans 
+
