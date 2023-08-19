f0a352a2d04a246768af01c7a77347b3a04c0d01
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-08-19 16:03:59 +0530

    Time: 381 ms (70.63%), Space: 17.5 MB (99.95%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,36 +1,57 @@
 # My solution O(log(max(piles))*n) time O(1) space
-import math
-class Solution:
-    def minEatingSpeed(self, piles: List[int], actual_hrs: int) -> int:
-        # maximum k can be max(piles)
-        # minimum k can be 1
+# import math
+# class Solution:
+#     def minEatingSpeed(self, piles: List[int], actual_hrs: int) -> int:
+#         # maximum k can be max(piles)
+#         # minimum k can be 1
         
-        # find optimal minimum k
+#         # find optimal minimum k
         
-        # binary search in this range to fix value of k
-        # sum of ceil of division of pile/k
-        # sum > h, then l = mid + 1
-        # sum <= h, store mid, h = mid - 1
+#         # binary search in this range to fix value of k
+#         # sum of ceil of division of pile/k
+#         # sum > h, then l = mid + 1
+#         # sum <= h, store mid, h = mid - 1
         
         
-        l = 1
-        h = max(piles)
-        ans = None
+#         l = 1
+#         h = max(piles)
+#         ans = None
         
-        while l<=h:
-            mid = l + (h-l)//2
+#         while l<=h:
+#             mid = l + (h-l)//2
             
-            division = [math.ceil(pile/mid) for pile in piles]
-            # print(l, h, mid)
-            # print(division)
-            hrs = sum(division)
-            # print(f"hrs: {hrs}, h: {h}")
-            if hrs > actual_hrs:
-                l = mid + 1
+#             division = [math.ceil(pile/mid) for pile in piles]
+#             # print(l, h, mid)
+#             # print(division)
+#             hrs = sum(division)
+#             # print(f"hrs: {hrs}, h: {h}")
+#             if hrs > actual_hrs:
+#                 l = mid + 1
                 
-            else:
-                h = mid - 1
-                ans = mid
+#             else:
+#                 h = mid - 1
+#                 ans = mid
             
         
-        return ans
+#         return ans
+
+
+# neetcode solution like my solution
+# O(log(max(piles))*n) time O(1) space
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = max(piles)
+
+        while l <= r:
+            k = (l + r) // 2
+
+            totalTime = 0
+            for p in piles:
+                totalTime += math.ceil(p / k)
+            if totalTime <= h:
+                res = min(res, k)
+                r = k - 1
+            else:
+                l = k + 1
+        return res
