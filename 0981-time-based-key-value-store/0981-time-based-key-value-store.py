58c72f7583b0547fe7f1c33226b4a5c4d02b5000
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-08-20 21:03:22 +0530

    Time: 689 ms (68.61%), Space: 74.6 MB (46.94%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,41 +1,44 @@
 # My solution 
-# Olog(timestamps)
-from collections import defaultdict
-class TimeMap:
+# set 
+# O(n calls)
+# get
+# O(m calls * log(L length of values for a key))
+# from collections import defaultdict
+# class TimeMap:
     
 
-    def __init__(self):
-        self.ds = defaultdict(list)
+#     def __init__(self):
+#         self.ds = defaultdict(list)
 
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        self.ds[key].append({timestamp: value})
-        
-
-    def get(self, key: str, timestamp: int) -> str:
-        
-        # Edge case when timestamp is small than first timestamp
-        if len(self.ds[key]) == 0 or timestamp < list(self.ds[key][0].keys())[0]:
-            return ""
+#     def set(self, key: str, value: str, timestamp: int) -> None:
+#         self.ds[key].append({timestamp: value})
         
 
-        l = 0
-        h = len(self.ds[key]) - 1
+#     def get(self, key: str, timestamp: int) -> str:
         
-        mid = None
-        while l<=h:
-            mid = (l+h)//2
-            # print(type(self.ds[key][mid].keys()))
-            if list(self.ds[key][mid].keys())[0] == timestamp:
-                return list(self.ds[key][mid].values())[0]
-            elif list(self.ds[key][mid].keys())[0] < timestamp:
-                l = mid + 1
-            else:
-                h = mid - 1
+#         # Edge case when timestamp is small than first timestamp
+#         if len(self.ds[key]) == 0 or timestamp < list(self.ds[key][0].keys())[0]:
+#             return ""
         
-        # edge case when mid points to higher element than target after binary search
-        if list(self.ds[key][mid].keys())[0] > timestamp:
-            mid -= 1
-        return list(self.ds[key][mid].values())[0]
+
+#         l = 0
+#         h = len(self.ds[key]) - 1
+        
+#         mid = None
+#         while l<=h:
+#             mid = (l+h)//2
+#             # print(type(self.ds[key][mid].keys()))
+#             if list(self.ds[key][mid].keys())[0] == timestamp:
+#                 return list(self.ds[key][mid].values())[0]
+#             elif list(self.ds[key][mid].keys())[0] < timestamp:
+#                 l = mid + 1
+#             else:
+#                 h = mid - 1
+        
+#         # edge case when mid points to higher element than target after binary search
+#         if list(self.ds[key][mid].keys())[0] > timestamp:
+#             mid -= 1
+#         return list(self.ds[key][mid].values())[0]
             
         
 
@@ -44,3 +47,32 @@ class TimeMap:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
+
+# neetcode solution
+# Set
+# O(n calls)
+# get
+# O(m calls * log(L length of values for a key))
+class TimeMap:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.keyStore = {}  # key : list of [val, timestamp]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keyStore:
+            self.keyStore[key] = []
+        self.keyStore[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res, values = "", self.keyStore.get(key, [])
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+        return res
