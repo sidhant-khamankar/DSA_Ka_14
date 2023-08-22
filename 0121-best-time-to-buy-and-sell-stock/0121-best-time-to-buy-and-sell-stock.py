a0cdc935464bf39c73b65ecc7786123d6d6706fe
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-08-22 22:41:30 +0530

    Time: 829 ms (89.72%), Space: 27.4 MB (60.29%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,12 @@
+# did not get
+# neetcode solution On time O1 space like striver, leetcode solution
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        
+        lowest = prices[0]
+        for price in prices:
+            if price < lowest:
+                lowest = price
+            res = max(res, price - lowest)
+        return res
