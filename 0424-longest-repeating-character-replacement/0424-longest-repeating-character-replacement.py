18a54bfcece3d2e7205d6f52f2bdaa116b06bfd7
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-09-02 00:35:04 +0530

    Time: 125 ms (52.74%), Space: 16.3 MB (95.85%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,3 +1,7 @@
+# did not get
+
+# neetcode solution O(n*26) time O(26) space
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
@@ -24,59 +28,3 @@ class Solution:
 
         # return (r - l + 1)
         return res
-
-
-
-# class Solution:
-#     def characterReplacement(self, s: str, k: int) -> int:
-#         # think about skipping of resetting current repeat count and moving forward
-        
-#         # org_k = k
-#         # max_repeating_len = 0
-        
-#         # recur(count, k, index, curr_char)
-
-        
-#         # if k = 0 or index >= len(s)
-#             # return count 
-            
-        
-        
-#         # count = 0
-#         # curr_char = ''
-#         # traverse string s with character c
-        
-#             # if curr_char = ""
-#                 # count ++
-#                 # curr_char = c
-#             # else if c == curr_char
-#                 # count++
-#             # else
-#                 # if k > 0
-#                     # max_repeating_len = max(max_repeating_len, recur(count+1, k - 1, index+1))
-#                 # curr_char = c
-#                 # max_repeating_len = max(max_repeating_len, recur(1, org_k, index + 1))
-                
-        
-#         org_k = k
-#         max_repeating_len = 0
-        
-#         # print("k, index, curr_char, count")
-#         def recur(count, k, index, curr_char):
-#             nonlocal max_repeating_len
-#             # print(k, index, curr_char, count)
-
-#             for i in range(index, len(s)):
-#                 if s[i] == curr_char:
-#                     count += 1
-#                     max_repeating_len = max(max_repeating_len, count)
-#                 else:
-#                     if k>0:
-#                         recur(count + 1, k-1, index + 1, curr_char)
-#                     recur(1, org_k, index + 1, s[i])
-        
-        
-#         recur(1, org_k, 1, s[0])
-#         return max_repeating_len 
-                
-        
