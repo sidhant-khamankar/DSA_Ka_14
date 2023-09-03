83f2f4329a6c2ecb01b6c51dd8822e049ea8be97
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-09-03 23:19:52 +0530

    Time: 1357 ms (52.51%), Space: 32.9 MB (78.50%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,33 +1,66 @@
-import heapq
+# Neetcode solution
+# O(n) time O(n) space
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # curr_window_set = set()
-        max_heap = []
-        ans = []
-        l = 0 
-        for r in range(len(nums)):
-            # print(l,r, max_heap)
-            if (r-l+1) > k:
-                # print("-^-")
-                max_in_curr_window, max_in_curr_window_index = heapq.heappop(max_heap)
+        output = []
+        q = collections.deque()  # indexes
+        l = r = 0
+        # O(n) O(n)
+        while r < len(nums):
+            # pop values from end which are smaller than current value in deque
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
                 
-                while max_in_curr_window_index < l:
-                    max_in_curr_window, max_in_curr_window_index = heapq.heappop(max_heap)
-                
-                heapq.heappush(max_heap, (max_in_curr_window, max_in_curr_window_index))
-                ans.append(-1 * max_in_curr_window)
-                l += 1
-                # curr_window_set.remove([l, nums[l]])
+            # add current value to deque
+            q.append(r)
+
+            # if window is out of bounds
+            # remove left val from window
+            if l > q[0]:
+                q.popleft()
             
-            # curr_window_set.add([l, nums[l]])
-            heapq.heappush(max_heap, (-1*nums[r], r))
-        
-        max_in_curr_window, max_in_curr_window_index = heapq.heappop(max_heap)
+            # skip untill end of first window is reached
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+
+        return output
+
+
+# My solution Max heap
+# High time complexity
+# On^2 time On/k space
+# import heapq
+# class Solution:
+#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+#         # curr_window_set = set()
+#         max_heap = []
+#         ans = []
+#         l = 0 
+#         for r in range(len(nums)):
+#             # print(l,r, max_heap)
+#             if (r-l+1) > k:
+#                 # print("-^-")
+#                 max_in_curr_window, max_in_curr_window_index = heapq.heappop(max_heap)
                 
-        while max_in_curr_window_index < l:
-            max_in_curr_window, max_in_curr_window_index = heapq.heappop(max_heap)
-        ans.append(-1 * max_in_curr_window)
-        return ans
+#                 while max_in_curr_window_index < l:
+#                     max_in_curr_window, max_in_curr_window_index = heapq.heappop(max_heap)
+                
+#                 heapq.heappush(max_heap, (max_in_curr_window, max_in_curr_window_index))
+#                 ans.append(-1 * max_in_curr_window)
+#                 l += 1
+#                 # curr_window_set.remove([l, nums[l]])
+            
+#             # curr_window_set.add([l, nums[l]])
+#             heapq.heappush(max_heap, (-1*nums[r], r))
+        
+#         max_in_curr_window, max_in_curr_window_index = heapq.heappop(max_heap)
+                
+#         while max_in_curr_window_index < l:
+#             max_in_curr_window, max_in_curr_window_index = heapq.heappop(max_heap)
+#         ans.append(-1 * max_in_curr_window)
+#         return ans
             
                            
             
