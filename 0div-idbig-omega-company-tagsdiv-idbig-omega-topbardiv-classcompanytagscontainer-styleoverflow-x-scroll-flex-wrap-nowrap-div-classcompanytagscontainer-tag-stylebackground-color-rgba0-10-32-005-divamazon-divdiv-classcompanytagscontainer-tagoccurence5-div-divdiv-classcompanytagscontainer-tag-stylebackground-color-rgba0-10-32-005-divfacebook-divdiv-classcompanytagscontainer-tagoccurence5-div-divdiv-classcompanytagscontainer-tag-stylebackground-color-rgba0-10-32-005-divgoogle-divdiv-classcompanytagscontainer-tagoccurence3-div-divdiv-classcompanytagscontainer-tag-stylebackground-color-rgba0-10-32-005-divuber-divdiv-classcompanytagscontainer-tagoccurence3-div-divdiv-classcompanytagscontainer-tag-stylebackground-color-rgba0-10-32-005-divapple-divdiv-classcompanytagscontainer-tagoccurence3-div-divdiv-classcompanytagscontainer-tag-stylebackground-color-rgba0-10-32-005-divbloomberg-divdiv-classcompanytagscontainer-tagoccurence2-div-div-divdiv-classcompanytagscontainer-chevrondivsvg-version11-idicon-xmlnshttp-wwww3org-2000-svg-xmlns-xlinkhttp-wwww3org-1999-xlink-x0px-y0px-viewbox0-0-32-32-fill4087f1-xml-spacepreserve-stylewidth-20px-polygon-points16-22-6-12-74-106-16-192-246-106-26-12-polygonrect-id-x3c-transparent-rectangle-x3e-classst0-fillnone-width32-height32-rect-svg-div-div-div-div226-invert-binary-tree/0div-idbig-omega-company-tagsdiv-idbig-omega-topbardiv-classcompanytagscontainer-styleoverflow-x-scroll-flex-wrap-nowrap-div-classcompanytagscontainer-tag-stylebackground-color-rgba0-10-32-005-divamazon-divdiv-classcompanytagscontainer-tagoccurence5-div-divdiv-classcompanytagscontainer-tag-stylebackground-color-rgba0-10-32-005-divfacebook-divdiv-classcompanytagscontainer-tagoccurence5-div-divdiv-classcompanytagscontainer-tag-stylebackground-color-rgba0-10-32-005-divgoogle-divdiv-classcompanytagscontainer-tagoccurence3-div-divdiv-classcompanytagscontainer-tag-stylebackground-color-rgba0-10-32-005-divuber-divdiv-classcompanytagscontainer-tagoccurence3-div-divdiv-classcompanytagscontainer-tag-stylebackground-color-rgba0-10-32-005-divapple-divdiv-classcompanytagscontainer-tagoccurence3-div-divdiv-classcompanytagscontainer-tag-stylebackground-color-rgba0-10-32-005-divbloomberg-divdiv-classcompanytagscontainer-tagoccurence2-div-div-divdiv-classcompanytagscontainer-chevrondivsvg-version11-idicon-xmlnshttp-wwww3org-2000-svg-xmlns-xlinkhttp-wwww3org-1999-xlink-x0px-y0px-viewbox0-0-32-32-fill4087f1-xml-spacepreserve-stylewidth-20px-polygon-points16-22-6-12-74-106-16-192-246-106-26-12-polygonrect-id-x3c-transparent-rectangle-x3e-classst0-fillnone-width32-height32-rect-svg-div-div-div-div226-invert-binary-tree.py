0c7d93e3849bc2f98be51a989009aa8622f19bae
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-10-08 16:06:39 +0530

    Time: 47 ms (5.98%), Space: 16.1 MB (82.64%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div226-invert-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div226-invert-binary-tree.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div226-invert-binary-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div226-invert-binary-tree.py
@@ -4,18 +4,113 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# My solution, Neetcode solution
+# O(n) time O(n) recursion stack space
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
-        return self.recur(root)
-    
-    def recur(self, root):
-        
         if not root:
-            return root
+            return None
         
-        root.left, root.right = self.recur(root.right), self.recur(root.left)
+        # swap the children
+        root.left, root.right = root.right, root.left
         
+        # make 2 recursive calls
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
+
+    
+# My solution, Leetcode solution
+
+# ```
+# /*
+#  * @lc app=leetcode id=226 lang=cpp
+#  *
+#  * [226] Invert Binary Tree
+#  */
+
+# // @lc code=start
+# /**
+#  * Definition for a binary tree node.
+#  * struct TreeNode {
+#  *     int val;
+#  *     TreeNode *left;
+#  *     TreeNode *right;
+#  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+#  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+#  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+#  * };
+#  */
+# /**
+#  * Definition for a binary tree node.
+#  * struct TreeNode {
+#  *     int val;
+#  *     TreeNode *left;
+#  *     TreeNode *right;
+#  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+#  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+#  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+#  * };
+#  */
+# class Solution
+# {
+# public:
+#     // My solution On time On space in worst case On leaves at level
+#     // Leetcode solution iterative
+#     TreeNode *invertTree(TreeNode *root)
+#     {
+#         queue<TreeNode *> q;
+
+#         if (root == NULL)
+#         { // edge case
+#             return root;
+#         }
+#         q.push(root);
+#         while (!q.empty())
+#         {
+#             TreeNode *curr = q.front();  // get front of the queue
+#             q.pop();                     // pop the front of queue
+#             TreeNode *left = curr->left; // store left and right child pointers
+#             TreeNode *right = curr->right;
+
+#             curr->left = right; // swap left and right childs
+#             curr->right = left;
+
+#             if (left != NULL) // push only when left or right are not null
+#                 q.push(left);
+#             if (right != NULL)
+#                 q.push(right);
+#         }
+
+#         return root;
+#     }
+# };
+# // @lc code=end
+
+# ```
+
+# Lead coding by fraz solution, leetcode solution
+
+# ```
+#    // Lead Coding by fraz solution On time Oh function calls space where h is height of tree
+#     // in worst case h can be n when tree is skewed
+#     // Leetcode solution recursive
+#     // void help(TreeNode *root)
+#     // {
+#     //     if (root == NULL)
+#     //         return;
+#     //     help(root->left);
+#     //     help(root->right);
+#     //     auto temp = root->left;
+#     //     root->left = root->right;
+#     //     root->right = temp;
+#     // }
+#     // TreeNode *invertTree(TreeNode *root)
+#     // {
+#     //     help(root);
+#     //     return root;
+#     // }
+# ```
         
         
