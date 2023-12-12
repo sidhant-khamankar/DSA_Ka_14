77803156cfeb08dd51744c9d69826a9e21e11556
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-12-12 23:50:35 +0530

    Time: 210 ms (17.94%), Space: 35.2 MB (8.71%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -5,20 +5,38 @@
 #         self.left = left
 #         self.right = right
 
-# My solution On time On recursion space
+# neetcode solution like my solution
+# On time On recursion space
+
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        ans = 0
-        def dfs(node, max_till_now):
-            nonlocal ans
-            if max_till_now <= node.val:
-                ans += 1
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+            return res
+
+        return dfs(root, root.val)
+
+# My solution On time On recursion space
+# using non local variable
+# class Solution:
+#     def goodNodes(self, root: TreeNode) -> int:
+#         ans = 0
+#         def dfs(node, max_till_now):
+#             nonlocal ans
+#             if max_till_now <= node.val:
+#                 ans += 1
             
-            if node.left:
-                dfs(node.left, max(max_till_now, node.val))
-            if node.right:
-                dfs(node.right, max(max_till_now, node.val))
+#             if node.left:
+#                 dfs(node.left, max(max_till_now, node.val))
+#             if node.right:
+#                 dfs(node.right, max(max_till_now, node.val))
         
-        dfs(root, root.val)
-        return ans
+#         dfs(root, root.val)
+#         return ans
             
