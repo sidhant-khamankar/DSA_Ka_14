0b09a8397d62d6cc32b40c0e1dca66b0386a2c34
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-12-13 15:35:31 +0530

    Time: 56 ms (12.39%), Space: 18.8 MB (62.72%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# got wrong ans multiple times
+# was just checking root value and one level above root's value
+# binary search tree concept is subsequent subtress should have limits as per higher level roots values
+
+# neetcode solution
+# On time On recursion space
+# pass left and right constraints for each binary search tree node
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        def valid(node, left, right):
+            if not node:
+                return True
+            if not (left < node.val < right):
+                return False
+
+            return valid(node.left, left, node.val) and valid(
+                node.right, node.val, right
+            )
+
+        return valid(root, float("-inf"), float("inf"))
