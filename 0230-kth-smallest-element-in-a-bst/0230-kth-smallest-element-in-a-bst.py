56a6b990e592fcd836be2535b1d4ed8df6040732
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-12-15 15:15:58 +0530

    Time: 51 ms (72.76%), Space: 20.3 MB (94.8%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -5,25 +5,48 @@
 #         self.left = left
 #         self.right = right
 
+# neetcode solution, using bst property
+# On time On recursion space
+# iterative solution but same complexity as recursive solution
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        stack = []
+        curr = root
+
+        # inorder
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            k -= 1
+            if k == 0:
+                return curr.val
+            
+            curr = curr.right
+
+
+# my solution Onlogn time On recursion space 
+# ignored the fact that smaller element start from bottom left
 # iterate through whole tree and push elements to list
 # sort list
 
 
-class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        elements = []
+# class Solution:
+#     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+#         elements = []
 
-        def dfs(node):
-            nonlocal elements
-            elements.append(node.val)
-            if node.left:
-                dfs(node.left)
-            if node.right:
-                dfs(node.right)
+#         def dfs(node):
+#             nonlocal elements
+#             elements.append(node.val)
+#             if node.left:
+#                 dfs(node.left)
+#             if node.right:
+#                 dfs(node.right)
         
-        dfs(root)
-        elements.sort()
-        # print(elements)
-        return elements[k-1]
+#         dfs(root)
+#         elements.sort()
+#         return elements[k-1]
 
 
