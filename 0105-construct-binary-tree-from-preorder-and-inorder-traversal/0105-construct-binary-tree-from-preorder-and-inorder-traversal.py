4bfa9d2680545916baca6748ff8d59ad2c0fb301
Sidhant Khamankar <avatarmeher999@gmail.com>
2023-12-16 14:32:28 +0530

    Time: 154 ms (60.39%), Space: 90.7 MB (49.67%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -16,7 +16,14 @@ class Solution:
             return None
 
         root = TreeNode(preorder[0])
+        # first element of preorder always contains root
+
+
         mid = inorder.index(preorder[0])
+        # find root in inorder list and pass its left portion and corresponding same number of elements from preorder list for creating left subtree
+        # similarly pass right portion of inorder list and corresponding leftover elements from preorder list for creating right subtree
+
         root.left = self.buildTree(preorder[1 : mid + 1], inorder[:mid])
         root.right = self.buildTree(preorder[mid + 1 :], inorder[mid + 1 :])
         return root
+
